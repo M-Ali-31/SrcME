e5a66bbe7d238eded4962dfcc5963d63dbe5eccd
M Ali <m.ali.cs31@gmail.com>
2015-07-27 23:12:55 +0500

    Added license notice to file

--- Data.cs
+++ Data.cs
@@ -1,4 +1,4 @@
-﻿/*  Copyright (C) 2015  M. Ali & M. Shoaib
+﻿/*  
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -12,7 +12,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+Copyright (C) 2015  M. Ali & M. Shoaib
 */
 
 using System;
--- OutputData.cs
+++ OutputData.cs
@@ -1,6 +1,5 @@
-﻿/*  Copyright (C) 2015  M. Ali & M. Shoaib
-
-    This program is free software: you can redistribute it and/or modify
+﻿/*  
+   This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -13,6 +12,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+Copyright (C) 2015  M. Ali & M. Shoaib
 */
 
 using System;
